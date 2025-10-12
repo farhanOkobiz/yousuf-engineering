60a7d9c0b8b293f93a61d704b68d8ea86ab31fd3
shohid-okobiz <dev.okobiz@gmail.com>
2025-10-12 15:31:20 +0600

    added url

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,7 +28,7 @@ exports.singupController = catchAsync(async (req, res) => {
   user.verificationTokenExpires = Date.now() + 10 * 24 * 60 * 60 * 1000; // Token valid for 10 days
   await user.save({ validateBeforeSave: false });
 
-  const url = `http://localhost:5173/verify?token=${verificationToken}`;
+  const url = `https://yousufengineering.com/verify?token=${verificationToken}`;
   await new Email(user, url).sendWelcome();
 
   createSendToken(user, 201, res);
